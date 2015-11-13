6702723595b3ace4a0597db2cbe16a8074812668
wytanj <wytanj@gmail.com>
2015-11-13 10:57:01 +0800

    formatting

--- unmain/timer.js
+++ unmain/timer.js
@@ -1,23 +1,19 @@
 var direction = 1
-setInterval(function ()
-{
+setInterval(function () {
   var logo = document.querySelector('div')
   var distance = parseInt(logo.style.top || '0', 10)
-  if (distance > 100 || distance < 0)
-  { direction = direction * -1 }
+  if (distance > 100 || distance < 0) { direction = direction * -1 }
 
   logo.style.top =
     logo.style.left =
-    distance + direction * 10 + 'px'
+      distance + direction * 10 + 'px'
 }, 100)
 
-window.requestAnimationFrame(function step()
-{
+window.requestAnimationFrame(function step () {
   var logo = document.querySelector('div')
   var topPos = parseInt(logo.style.top || '0', 10)
   var leftPos = parseInt(logo.style.left || '0', 10)
-  if(topPos > 100 || topPos < 0)
-  {direction = direction * -1}
+  if (topPos > 100 || topPos < 0) {direction = direction * -1}
   logo.style.top =
     topPos + direction * 10 + 'px'
   logo.style.left =
